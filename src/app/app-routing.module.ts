57d174c39b3a97c65b3e15d11fd74a6935fe02fb
soudani lina <lina.soudani@benchmatrix.ca>
2021-12-21 20:33:10 +0100

    added readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDetailsComponent } from './add-details/add-details.component';
 import { Child1Component } from './child1/child1.component';
 import { Child2Component } from './child2/child2.component';
+import { ParentComponent } from './parent/parent.component';
 import { Sibling1Component } from './task5/sibling1/sibling1.component';
 import { Sibling2Component } from './task5/sibling2/sibling2.component';
 
@@ -12,6 +14,8 @@ const routes: Routes = [
     { path: 'sibling2', component: Sibling2Component },
     { path: 'child1', component: Child1Component },
     { path: 'child2', component: Child2Component },
+    { path: 'add-details', component: AddDetailsComponent },
+    { path: 'parent', component: ParentComponent },
   
 ];
 
